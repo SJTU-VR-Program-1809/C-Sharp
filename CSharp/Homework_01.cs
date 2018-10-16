de6ad363c3c19dbdd6d292102d691066e46db35a
PUMIAO ZHOU <597513835@qq.com>
2018-10-16 21:24:40 +0800

    0.1

--- CSharp/Homework_01.cs
+++ CSharp/Homework_01.cs
@@ -7,11 +7,21 @@ namespace CSharp
         static void Main(string[] args)
         {
             //求这两组数的逆序数对
-
+            int reverse_num_a = 0;
+            int reverse_num_b = 0;
             int[] a = { 1, 2, 3, 4, 5, 6 };
             
             int[] b = { 3, 1, 4, 2, 6, 7 };
-            Console.WriteLine("zqx");
+            for (int j = 0; j < 6; j++)
+            {
+                for (int k = 0; k <j; k++)
+                {
+                    if (a[j] < a[k]) { reverse_num_a++; }
+                    if (b[j] < b[k]) { reverse_num_b++; }
+                }
+            }
+            Console.WriteLine("zqx a: "+ reverse_num_a);
+            Console.WriteLine("zqx b: " + reverse_num_b);
             Console.ReadKey();
         }
     }
