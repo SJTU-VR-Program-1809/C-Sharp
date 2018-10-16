4f5af5732a0e5d4fe6d4057e9751b2f69ef4b61e
E <helloestar@gmail.com>
2018-10-16 23:52:25 +0800

    修复作业内容

--- CSharp/Homework_01.cs
+++ CSharp/Homework_01.cs
@@ -6,42 +6,9 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            //求这两组数的逆序数对
             int[] a = { 1, 2, 3, 4, 5, 6 };
-            PrintArray(a);
-            GetReverseNumber(a);
-
             int[] b = { 3, 1, 4, 2, 6, 7 };
-            PrintArray(b);
-            GetReverseNumber(b);
-            
-        }
-        static void PrintArray(int[] a)
-        {
-            foreach (var i in a)
-            {
-                Console.Write(i.ToString() + " ");
-            }
-            Console.WriteLine();
-        } 
-        static void GetReverseNumber(int[] a)
-        {
-            int i = 0, j = 1;
-
-            while (i < 6)
-            {
-                while (i < j && j < 6)
-                {
-                    if (a[i] > a[j])
-                    {
-                        Console.WriteLine("(" + a[i] + "," + a[j] + ")");
-                    }
-                    j++;
-                }
-
-                i++;
-                j = i + 1;
-            }
-
         }
     }
 }
