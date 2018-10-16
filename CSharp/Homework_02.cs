b4bd2304d34662c6265f9d5c2c8fb2ed8a353c9b
architart <1437830419@qq.com>
2018-10-16 23:36:31 +0800

    homework

---
+++ CSharp/Homework_02.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Homework_01
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6 };
+            PrintArray(a);
+            GetReverseNumber(a);
+
+            int[] b = { 3, 1, 4, 2, 6, 7 };
+            PrintArray(b);
+            GetReverseNumber(b);
+            
+        }
+        static void PrintArray(int[] a)
+        {
+            foreach (var i in a)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+            Console.WriteLine();
+        } 
+        static void GetReverseNumber(int[] a)
+        {
+            int i = 0, j = 1;
+
+            while (i < 6)
+            {
+                while (i < j && j < 6)
+                {
+                    if (a[i] > a[j])
+                    {
+                        Console.WriteLine("(" + a[i] + "," + a[j] + ")");
+                    }
+                    j++;
+                }
+
+                i++;
+                j = i + 1;
+            }
+
+        }  
+    }
+}
