1c6ec685fc68ad21e8b3c78861641d4c170e0b71
1735008279 <1735008279@qq.com>
2018-10-27 20:00:41 +0800

    C

---
+++ CSharp/HomeWork02.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Determinant
+{
+    class determinant
+    {
+        private List<int[]> D = new List<int[]>(); //Storage determinant
+        public void Create() //Determinant assignment
+        {
+            D.Clear();
+            D.Add(new int[] { 2, 1,-5, 7,2});
+            D.Add(new int[] { 8,-3, 0, 8,3});
+            D.Add(new int[] { 5, 0,-3, 9,4});
+            D.Add(new int[] { 2, 0, 0, 9,5});
+            D.Add(new int[] { 1, 0, 0, 0,6});
+
+        }
+        public int Get(int index_x, int index_y) //Get elements of line X-1, column Y-1
+        {
+            return D[index_x - 1][index_y - 1];
+        }
+        public int GetCount() //Get the number of determinant rows (columns)
+        {
+            return D.Count;
+        }
+        public void Display(double Dvalue) //Console formatting display determinant
+        {
+            for (int i = 0; i < D.Count; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < D[i].Length; j++)
+                {
+                    Console.Write(j == D[i].Length - 1 ? (D[i][j]).ToString() : (D[i][j] + ",").ToString());
+                }
+                Console.Write("|");
+                if (D.Count % 2 == 0)
+                {
+                    if (i == D.Count / 2 - 1)
+                    {
+                        Console.Write("=" + Dvalue);
+                    }
+                }
+                else if (i == (D.Count - 1) / 2)
+                {
+                    Console.Write("=" + Dvalue);
+                }
+                Console.WriteLine();
+            }
+        }
+        public  double GetHls(determinant D) //A determinant data is passed in, and its value is calculated and returned.
+        {
+            int[] arr = CollSet(D.GetCount());
+            List<int[]> list = new List<int[]>();
+            FullArray(list, arr, 0);
+            double sum = 0;
+            for (int cn = 1; cn <= list.Count; cn++)
+            {
+                double xg = 1;
+                for (int i = 1; i <= D.GetCount(); i++)
+                {
+                    xg *= D.Get(i, list[cn - 1][i - 1]);
+                }
+                sum += Math.Pow(-1, N(list[cn - 1])) * xg;
+            }
+            return sum;
+        }
+
+        public  int[] CollSet(int num) //Generating continuous number arrangement
+        {
+            int[] result = new int[num];
+            for (int i = 0; i < num; i++) result[i] = i + 1;
+            return result;
+        }
+
+        public  int N(int[] arr) //Inverse number calculation
+        {
+            int N = 0;
+            for (int i = 1; i < arr.Length; i++) for (int j = 0; j < i; j++) if (arr[j] > arr[i]) N++;
+            return N;
+        }
+
+        public  void FullArray(List<int[]> list, int[] arr, int k) //Full Permutation generation
+        {
+            if (k == arr.Length)
+            {
+                int[] cp_arr = new int[arr.Length];
+                for (int i = 0; i < arr.Length; i++) cp_arr[i] = arr[i];
+                list.Add(cp_arr);
+            }
+            else
+            {
+                for (int i = k; i < arr.Length; i++)
+                {
+                    { int t = arr[k]; arr[k] = arr[i]; arr[i] = t; }
+                    FullArray(list, arr, k + 1);
+                    { int t = arr[k]; arr[k] = arr[i]; arr[i] = t; }
+                }
+            }
+        }
+
+
+    }
+}
--- CSharp/Homework_01.cs
+++ CSharp/Homework_01.cs
@@ -1,77 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CSharp
+namespace Determinant
 {
-    class Homework_01
+    class Program
     {
-        private static int x, y;
-        private static int a;
         static void Main(string[] args)
         {
-            int n = 123;
-            int ret = ReverseNumber(n);
-            Console.WriteLine($"Reverse 123 :{ret}");
-            n = -123;
-            ret = ReverseNumber(n);
-            Console.WriteLine($"Reverse -123:{ret}");
-            n = 123456789;
-            ret = ReverseNumber(n);
-            Console.WriteLine($"Reverse 123456789:{ret}");
-
-
-            int[] a = { 1, 2, 3, 4, 5, 6 };
-            PrintArray(a);
-
-            int count = GetReveseNumber(a);//0
-            Console.WriteLine($"The number of  reverse sequence is:{count}");
-
-            int[] b = { 3, 1, 4, 2, 6, 7 };
-            PrintArray(b);
-
-            count = GetReveseNumber(b);//3
-            Console.WriteLine($"The number of  reverse sequence is:{count}");
-        }
-
-        static void PrintArray(int[] a)
-        {
-            foreach (var i in a)
-            {
-                Console.Write(i.ToString() + " ");
-            }
-
-            Console.WriteLine();
-        }
-
-        static int GetReveseNumber(int[] a)
-        {
-            int c = 0;
-
-            for (int i = 0; i < a.Length - 1; i++)
-            {
-                for (int j = i; j < a.Length - 1; j++)
-                {
-                    if (a[i] > a[j + 1])
-                    {
-                        c++;
-                        Console.WriteLine("逆序数对为：" + a[i] + " " + a[j + 1]);
-                    }
-                }
-            }
-            return c;
-        }
-
-        static int ReverseNumber(int n)
-        {
-            int res = 0;
-            do
-            {
-                a = n % 10;
-                n /= 10;
-                res = res * 10 + a;
-            } while (n != 0);
-
-            return res;
-
+            determinant  D = new determinant(); //Creating determinant classes
+            D.Create(); 	   
+            D.Display(D.GetHls(D)); //Console formatting display determinant and its value
+            Console.ReadKey();
         }
     }
 }
