8d62f69a89a2d8c5caf8aeb5c261da768389cac6
chengxiangyu018 <chengxiangyu018@outlook.com>
2018-11-09 21:46:07 +0800

    LinkList

---
+++ CSharpHomeWork/LinkList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LinkList
+{
+    private Node head = new Node();
+    
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int i=0)
+        {
+            data = i;
+        }
+    }
+    public int Size { get; }
+    public LinkList(params int[] nums)
+    {
+        
+        if (nums ==null)
+        {
+            Console.WriteLine("链表为空");
+            return;
+        }
+        head.next = new Node(nums[0]);
+        Node p = head.next;
+       for (int i = 1; i < nums.Length; i++)
+       {
+            p.next = new Node(nums[i]);
+            p = p.next;
+       }
+        Size = nums.Length;
+    }
+    //if x is null, insert d as first node
+    public Node InsertBefore(Node x, int d)
+    {
+        Node p = new Node();
+        p = head;
+        while ( p.next!= x)
+         {
+            p = p.next;
+         }
+        x.next = p.next;
+        p.next = x;
+        x.data= d;
+        //Console.WriteLine(x.next.data);
+        return p;
+    }
+    public Node Insertafter(Node x, int d)
+    {
+        Node p = new Node();
+        p = head.next;
+        while (p!= x)
+         {
+            p = p.next;
+         }
+        x.next = p.next;
+        p.next = x;
+        x.data = d;
+        return p;
+    }
+    public Node Find(int d)
+    {
+        Node p = head;
+        while (p.data!= d)
+         {
+            p = p.next;
+        }
+        return p;
+    }
+    public void Delete(Node x)
+    {
+        Node p = head;
+        while (p.next!= x)
+         {
+            p = p.next;
+         }
+        p.next = x.next;
+        x.next = null;
+    }
+    //remove all nodes contain d
+    public void Delete(int d)
+    {
+        Node p = head;
+        while (p!= null)
+        {
+            p = p.next;
+            if (p.data == d)
+                p.data = 0;
+        }
+    }
+    public void Display()
+    {
+        Console.WriteLine("链表如下：");
+        Node p = head.next;
+        if (p == null)
+        {
+            Console.WriteLine("这是一个空链表");
+            return;
+        }
+        while (p!= null)
+        {
+            Console.Write(p.data);
+            p = p.next;
+        }
+    }
+        //翻转public LinkList Reverse();
+ }
+
--- CSharpHomeWork/Program.cs
+++ CSharpHomeWork/Program.cs
@@ -4,49 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace nixushu
+
+namespace HomeWork2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] a = { 1, 2, 3, 4, 5, 6 };
-            PrintArray(a);
+            int[] num = { 1, 3, 5, 7 };
+            LinkList list = new LinkList(num);
+            LinkList.Node x = list.Find(3);
+            var n1 = list.InsertBefore(x, 10);
+            list.Display();
+            list.Insertafter(n1, 3);
+            list.Display();
+            list.Delete(3);
+            list.Display();
 
-            GetReveseNumber(a);
-            int[] b = { 3, 1, 4, 2, 6, 7 };
-            PrintArray(b);
-
-            GetReveseNumber(b);
-            Console.ReadKey();
-        }
-
-        static void PrintArray(int[] a)
-        {
-            foreach (var i in a)
-            {
-                Console.Write(i.ToString() + " ");
-            }
-            Console.WriteLine();
-        }
-
-        static void GetReveseNumber(int[] a)
-        {
-            int count = 0;
-            for (int i = 0; i < a.Length; i++)
-            {
-                for (int j = i + 1; j < a.Length - 1; j++)
-                {
-                    if (a[i] > a[j])
-                    {
-                        Console.Write("({0},{1})", a[i], a[j]);
-                        count += 1;
-                    }
-                }
-            }
-            Console.WriteLine(@"
-逆序数对数为{0}", count);
+            
         }
     }
 }
-
